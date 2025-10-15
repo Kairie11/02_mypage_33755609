453cb189f6a7978221939886135271e93b297a1f
Berkei Koranteng <dkora001@gold.ac.uk>
2025-10-15 17:15:19 +0100

    initial

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.writeHead(200, { "Content-Type": "html" });
+    res.end("Hello World");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
