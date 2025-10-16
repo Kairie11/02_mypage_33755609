3f981ac0b5e8b16a21598e32438bb022555d4b5f
Berkei Koranteng <dkora001@gold.ac.uk>
2025-10-16 11:58:39 +0100

    "This is Daniel" added to res.end

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "html" });
-    res.end("Hello World");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World. This is Daniel.");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
